27fab4d5003fdf20b17d0fbd74510084291e0e08
Fyzxs <Fyzxs@users.noreply.github.com>
2018-05-15 15:57:12 -0700

    Disabling the console
    The console project is having to be updated with the changes to the rest of the code.
    While this is normally ideal; in this case, it's annoying. The console will be part of a full
    rewrite to find better UI abstractions.

--- ConsolePomodoro/Program.cs
+++ ConsolePomodoro/Program.cs
@@ -1,7 +1,4 @@
-﻿using PomodoroTimerLib.Library.Time.Interval;
-using PomodoroTimerLib.Library.Timers;
-using System;
-using System.Threading;
+﻿using System.Threading;
 
 namespace ConsolePomodoro
 {
@@ -11,87 +8,79 @@ namespace ConsolePomodoro
         private static void Main(string[] args)
         {
             // Start a thread or two to do some work...
-            ThreadPool.QueueUserWorkItem(o => BackgroundWork(WaitHandle));
 
-            // Block until our ManualResetEvent is set
-            WaitHandle.WaitOne();
-
-            Console.Clear();
-            Console.WriteLine("Timer Over!");
-            Console.WriteLine("Enter to Exit");
-            Console.ReadLine();
         }
 
-        private static void BackgroundWork(EventWaitHandle waitHandle)
-        {
-            RunSession();
-        }
+        //private static void BackgroundWork(EventWaitHandle waitHandle)
+        //{
+        //    RunSession();
+        //}
 
-        private static void RunBreak()
-        {
-            Console.Clear();
-            Console.WriteLine("Take a break!");
-            Console.WriteLine("Hit Enter to start your break:");
-            Console.ReadLine();
-            ICountdownTimer timer = new CountdownTimer(new Seconds(5), new Milliseconds(500));
-            timer.TimerEvent += (countdownTime, more) =>
-            {
-                if (more.AsBool())
-                {
-                    PrintRemainingBreak(countdownTime.Remaining());
-                    return;
-                }
-                timer.Stop();
-                WaitHandle.Set();
-            };
-            timer.Start();
-        }
+        //private static void RunBreak()
+        //{
+        //    Console.Clear();
+        //    Console.WriteLine("Take a break!");
+        //    Console.WriteLine("Hit Enter to start your break:");
+        //    Console.ReadLine();
+        //    ICountdownTimer timer = new CountdownTimer(new Seconds(5), new Milliseconds(500));
+        //    timer.TimerEvent += (countdownTime, more) =>
+        //    {
+        //        if (more.AsBool())
+        //        {
+        //            PrintRemainingBreak(countdownTime.Remaining());
+        //            return;
+        //        }
+        //        timer.Stop();
+        //        WaitHandle.Set();
+        //    };
+        //    timer.Start();
+        //}
 
-        private static void RunSession()
-        {
-            Console.Clear();
-            Console.WriteLine("Time for Pomodoro!");
-            Console.WriteLine("Hit Enter to start your session:");
-            Console.ReadLine();
-            ICountdownTimer timer = new CountdownTimer(new Seconds(5), new Milliseconds(500));
-            timer.TimerEvent += (countdownTime, more) =>
-            {
-                if (more.AsBool())
-                {
-                    PrintRemainingSession(countdownTime.Remaining());
-                    return;
-                }
-                timer.Stop();
-                RunBreak();
-            };
-            timer.Start();
-        }
+        //private static void RunSession()
+        //{
+        //    Console.Clear();
+        //    Console.WriteLine("Time for Pomodoro!");
+        //    Console.WriteLine("Hit Enter to start your session:");
+        //    Console.ReadLine();
+        //    ICountdownTimer timer = new CountdownTimer(new Seconds(5), new Milliseconds(500));
+        //    timer.TimerEvent += (countdownTime, more) =>
+        //    {
+        //        if (more.AsBool())
+        //        {
+        //            PrintRemainingSession(countdownTime.Remaining());
+        //            return;
+        //        }
+        //        timer.Stop();
+        //        RunBreak();
+        //    };
+        //    timer.Start();
+        //}
 
-        private static void PrintRemainingSession(TimeSpan timespan)
-        {
-            ConsoleColor prevFore = Console.ForegroundColor;
-            Console.SetCursorPosition(0, 0);
-            Console.ForegroundColor = ConsoleColor.Yellow;
-            Console.WriteLine("################################################################################");
-            Console.WriteLine("#####                    Quinn Gil's Pomodoro Session                      #####");
-            Console.WriteLine("################################################################################");
-            Console.ForegroundColor = ConsoleColor.Green;
-            Console.WriteLine(timespan.ToString(@"mm\:ss"));
-            Console.ForegroundColor = prevFore;
-        }
+        //private static void PrintRemainingSession(TimeSpan timespan)
+        //{
+        //    ConsoleColor prevFore = Console.ForegroundColor;
+        //    Console.SetCursorPosition(0, 0);
+        //    Console.ForegroundColor = ConsoleColor.Yellow;
+        //    Console.WriteLine("################################################################################");
+        //    Console.WriteLine("#####                    Quinn Gil's Pomodoro Session                      #####");
+        //    Console.WriteLine("################################################################################");
+        //    Console.ForegroundColor = ConsoleColor.Green;
+        //    Console.WriteLine(timespan.ToString(@"mm\:ss"));
+        //    Console.ForegroundColor = prevFore;
+        //}
 
-        private static void PrintRemainingBreak(TimeSpan timespan)
-        {
-            ConsoleColor prevFore = Console.ForegroundColor;
-            Console.SetCursorPosition(0, 0);
-            Console.ForegroundColor = ConsoleColor.Cyan;
-            Console.WriteLine("################################################################################");
-            Console.WriteLine("#####                     Quinn Gil's Pomodoro Break                       #####");
-            Console.WriteLine("################################################################################");
-            Console.ForegroundColor = ConsoleColor.Red;
-            Console.WriteLine(timespan.ToString(@"mm\:ss"));
-            Console.ForegroundColor = prevFore;
-        }
+        //private static void PrintRemainingBreak(TimeSpan timespan)
+        //{
+        //    ConsoleColor prevFore = Console.ForegroundColor;
+        //    Console.SetCursorPosition(0, 0);
+        //    Console.ForegroundColor = ConsoleColor.Cyan;
+        //    Console.WriteLine("################################################################################");
+        //    Console.WriteLine("#####                     Quinn Gil's Pomodoro Break                       #####");
+        //    Console.WriteLine("################################################################################");
+        //    Console.ForegroundColor = ConsoleColor.Red;
+        //    Console.WriteLine(timespan.ToString(@"mm\:ss"));
+        //    Console.ForegroundColor = prevFore;
+        //}
     }
 
     public sealed class Pomodoro
